ca9081d5d7134b86d9348bd72ce6646c97b8b6c2
gustavo-guarnieri-de-melo <gustavoguarnieridemelo@gmail.com>
2023-06-20 21:36:10 -0300

    Novo projeto alura

---
+++ Projeto React Alura/projeto-alura/src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" 
+            alt="O Banner principal da pagina do Organo" />
+        </header>
+    )
+}
